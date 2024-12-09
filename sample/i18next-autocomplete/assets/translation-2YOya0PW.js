99c52ea46db105c873cc0ed241614493ed733661
Crow281 <heretic@proton.me>
2024-12-08 17:11:06 -0800

    Samples

---
+++ sample/i18next-autocomplete/assets/translation-2YOya0PW.js
@@ -0,0 +1,1 @@
+const o={goodbye:"Adiós",goodDay:"¡Buenos días!",helloWorld:"¡Hola, mundo!"},a={example:o};export{a as default,o as example};
---
+++ sample/i18next-autocomplete/assets/translation-Cns19lIA.js
@@ -0,0 +1,1 @@
+const o={goodbye:"Goodbye",goodDay:"Good day!",helloWorld:"Hello world!"},l={example:o};export{l as default,o as example};
